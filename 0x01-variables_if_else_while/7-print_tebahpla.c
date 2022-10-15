2b0d4bd8668f2035c93a91c37888f972999dbabc
RENOVAT NYANGOMA <revocatusba@gmail.com>
2022-10-15 09:52:55 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry points
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+}

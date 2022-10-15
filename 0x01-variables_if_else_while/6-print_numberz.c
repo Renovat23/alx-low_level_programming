140f487fbc4ff04d3123c429b5ce033d2d37c26b
RENOVAT NYANGOMA <revocatusba@gmail.com>
2022-10-15 09:45:09 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry points
+ * Return: Always 0
+ */
+int main(0)
+{
+	int c;
+
+	c = 0;
+	while
+		(c < 10) {
+			putchar(c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}

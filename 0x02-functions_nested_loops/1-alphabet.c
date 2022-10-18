5ed0279f0d955b6930024016efd60e19f31e4342
RENOVAT NYANGOMA <revocatusba@gmail.com>
2022-10-18 13:49:40 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - Function to print abc
  *
- * Return: Always 0.
+ * Return: 0
  */
 void print_alphabet(void)
 {
@@ -13,5 +13,5 @@ void print_alphabet(void)
 	{
 		_putchar(a);
 	}
-	putchar('\n');
+	_putchar('\n');
 }

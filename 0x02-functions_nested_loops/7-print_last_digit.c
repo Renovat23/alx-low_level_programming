cd7a0103f14ece878e558a192a4d0bf774195733
RENOVAT NYANGOMA <revocatusba@gmail.com>
2022-10-18 15:16:10 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit
+ * @n: The number
+ * Return: Value of last digit
+ */
+int print_last_digit(int n)
+{
+	int a = n % 10;
+
+	if (a < 0)
+		a *= -1;
+
+	_putchar(a + '0');
+	return (0);
+}

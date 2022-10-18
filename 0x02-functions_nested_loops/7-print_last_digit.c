953bf4de2580deef8f9ca3f85d70f285f28031fe
RENOVAT NYANGOMA <revocatusba@gmail.com>
2022-10-18 15:24:42 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,15 +3,16 @@
 /**
  * print_last_digit - print the last digit
  * @n: The number
- * Return: Value of last digit
+ * Return: Value of the last digit
  */
 int print_last_digit(int n)
 {
 	int a = n % 10;
 
 	if (a < 0)
-		a *= -1;
+		a <= -1;
 
 	_putchar(a + '0');
+
 	return (0);
 }

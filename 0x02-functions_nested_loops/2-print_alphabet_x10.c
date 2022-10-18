b18ed367ffd98e48bf7daea37d1156608db9045b
RENOVAT NYANGOMA <revocatusba@gmail.com>
2022-10-18 14:08:10 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int a;
+	char b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 'a'; b <= 'z'; b++)
+		{
+			_putchar(b);
+		}
+	}
+	_putchar('\n');
+}

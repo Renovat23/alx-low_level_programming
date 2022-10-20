f00735fdc938382bdb8feaa5011794c5dab28dec
RENOVAT NYANGOMA <revocatusba@gmail.com>
2022-10-20 13:54:42 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: the number of times the character should be printed
+ * Return: void
+ */
+void print_line(int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}

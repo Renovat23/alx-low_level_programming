5a5978e13911b602929b002b878e1171adcff041
RENOVAT NYANGOMA <revocatusba@gmail.com>
2022-10-20 11:47:17 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9, followed by a new line
- * printed number
- * Return: Always 0
+ * print_numbers - print 0123456789
+ *
+ * Return: void
  */
+
 void print_numbers(void)
 {
-	char c;
+	char a;
 
-	for (c = '0'; c <= '9'; c++)
+	for (a = '0'; a <= '9'; a++)
 	{
-		_putchar(c);
+		_putchar(a);
 	}
-	_putchar('\n')
+	_putchar('\n');
 }

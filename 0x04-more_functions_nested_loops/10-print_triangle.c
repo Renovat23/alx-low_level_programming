6ab903a032bb953b3cabb81cbff696d8a3c52c41
RENOVAT NYANGOMA <revocatusba@gmail.com>
2022-10-20 14:53:00 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,9 +3,11 @@
 /**
  * print_triangle - entry point
  * owned by Bwave/Bright Daniel
+ * Description: Prints diagonals
  * @size: size of the triangle
  * Return: void
  */
+
 void print_triangle(int size)
 {
 	int row, hashes, spaces;

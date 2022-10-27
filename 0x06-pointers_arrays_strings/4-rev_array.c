dcf4a9fbc6aea38e8d05e3df42fd96231cf5b048
RENOVAT NYANGOMA <revocatusba@gmail.com>
2022-10-27 11:29:57 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse array
+ * @a:array
+ * @n:integer
+ * Return:void
+ */
+void reverse_array(int *a, int n)
+{
+	int x, b;
+
+	for (x = 0; (x < (n - 1) / 2); x++)
+	{
+		b = a[i];
+		a[x] = a[n - 1 - x];
+		a[n - 1 - x] = b;
+	}
+}

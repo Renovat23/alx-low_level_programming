002d395ade502aec15179377877b276d4ea43d21
RENOVAT NYANGOMA <revocatusba@gmail.com>
2022-11-08 08:01:11 -0800

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,10 +16,13 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
+
 	array = (char *)malloc(sizeof(char) * size);
 	if (array == NULL)
 		return (NULL);
+
 	for (i = 0; i < size; i++)
 		array[i] = c;
+
 	return (array);
 }

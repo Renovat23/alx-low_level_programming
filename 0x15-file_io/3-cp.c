9a0cc2f5e7b9be55a184e3b0d2ef418d6cda1428
RENOVAT NYANGOMA <revocatusba@gmail.com>
2022-12-05 10:16:44 -0800

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -89,8 +89,8 @@ int main(int argc, char *argv[])
 	int fd_from, fd_to, close_to, close_from;
 	ssize_t lenr, lenw;
 	char buffer[1024];
-
 	mode_t file_perm;
+
 	check97(argc);
 	fd_from = open(argv[1], O_RDONLY);
 	check98((ssize_t)fd_from, argv[1], -1, -1);
